4604bd5744967b91e4176d68b611b5002bbeea2e
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2021-04-19 11:28:32 +0300

    Remove 'one'

--- src/Space.ts
+++ src/Space.ts
@@ -205,7 +205,7 @@ class SpaceService extends Base {
   }
 
   /**
-   * Get one billing plan of space by space name
+   * Get illing plan of space by space name
    */
   async getBillingPlan({ name }: { name: Space["name"] }) {
     const { apiEndpointAddress, got } = this.lensPlatformClient;
