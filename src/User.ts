558cd29961938e6762cf7c47ddb0ee8bd2141661
Mario Sarcher <mario@sarcher.de>
2021-04-27 13:11:00 +0200

    add getMany() for fetching users (#32)

--- src/User.ts
+++ src/User.ts
@@ -37,6 +37,14 @@ class UserService extends Base {
     return (json as unknown) as User;
   }
 
+  async getMany(queryString?: string): Promise<User[]> {
+    const { apiEndpointAddress, got } = this.lensPlatformClient;
+    const url = `${apiEndpointAddress}/users${queryString ? `/?${queryString}` : ""}`;
+    const json = await got.get(url);
+
+    return (json as unknown) as User[];
+  }
+
   async updateOne(user: Except<User, "username"> & { username: string }): Promise<User> {
     const { apiEndpointAddress, got } = this.lensPlatformClient;
     const url = `${apiEndpointAddress}/users/${user.username}`;
