b5ad8a16e36119a349974cdb9ed3d317ec548b67
Panu Horsmalahti <phorsmalahti@mirantis.com>
2021-05-25 11:12:32 +0300

    Email is optional in user update. (#56)

--- src/UserService.ts
+++ src/UserService.ts
@@ -57,9 +57,9 @@ class UserService extends Base {
   }
 
   /**
-   * Updating requires both username and email
+   * Update user
    */
-  async updateOne(username: string, user: Except<User, "email"> & { email: string } & { attributes?: UserAttributes }): Promise<User> {
+  async updateOne(username: string, user: User & { attributes?: UserAttributes }): Promise<User> {
     const { apiEndpointAddress, got } = this.lensPlatformClient;
     const url = `${apiEndpointAddress}/users/${username}`;
     const json = await got.patch(url, { json: user });
