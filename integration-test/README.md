49b4964a3be2eec8fb5f9161dc8eb760cbf2f264
Panu Horsmalahti <phorsmalahti@mirantis.com>
2021-05-27 11:56:38 +0300

    Expose credentials to the integration test in the CI.

--- integration-test/README.md
+++ integration-test/README.md
@@ -1,3 +1,5 @@
 # Integration tests
 
 The integration tests are executed against the staging environment. The GitHub Action secrets `STAGING_USERNAME` and `STAGING_PASSWORD` are available for a test user.
+
+The integration tests expect `STAGING_USERNAME` and `STAGING_PASSWORD` environmental variables when running the tests.
--- integration-test/UserService.test.ts
+++ integration-test/UserService.test.ts
@@ -1,5 +1,14 @@
 import { LensPlatformClient } from "../";
 
+const user = {
+  username: process.env.STAGING_USERNAME,
+  password: process.env.STAGING_PASSWORD
+};
+
+if (!user.username || !user.password) {
+  throw new Error("STAGING_USERNAME or STAGING_PASSWORD environmental variable not set");
+}
+
 describe("UserService", () => {
   describe("getOne", () => {
     it("can get itself", () => {
