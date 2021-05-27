fe28caca6aed4a4ae951b336be733c9ac96e161f
Alex Culliere <alozhkin@mirantis.com>
2021-05-27 17:50:48 +0300

    Fix descriptions for some item codes
    Signed-off-by: Alex Culliere <alozhkin@mirantis.com>

--- src/exceptions/HTTPErrrorCodes.ts
+++ src/exceptions/HTTPErrrorCodes.ts
@@ -34,8 +34,8 @@ export const HTTPErrorCodes = {
   504: "Gateway Timeout",
   505: "HTTP Version Not Supported",
   506: "Variant Also Negotiates",
-  507: "Insufficient Storage (WebDAV",
-  508: "Loop Detected (WebDAV",
+  507: "Insufficient Storage",
+  508: "Loop Detected",
   510: "Not Extended",
   511: "Network Authentication Required"
 };
