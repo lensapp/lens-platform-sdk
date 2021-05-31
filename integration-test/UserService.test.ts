5760728e7129e5687db6302c4c2ed039b63a77f4
Alex Culliere <alozhkin@mirantis.com>
2021-05-31 17:37:23 +0300

    Update and refactor UserService tests
    Signed-off-by: Alex Culliere <alozhkin@mirantis.com>

--- integration-test/UserService.test.ts
+++ integration-test/UserService.test.ts
@@ -1,107 +1,94 @@
-import LensPlatformClient from "../src/LensPlatformClient";
-import { ResourceOwnerPassword } from "simple-oauth2";
 import { config } from "./configuration";
+import { testPlatformClientFactory } from "./utils";
+import type { TestPlatformClient } from "./utils";
 import { BadRequestException, ForbiddenException, LensSDKException, NotFoundException, UnauthorizedException } from "../src/exceptions";
 
 describe("UserService", () => {
+  const mainUser = config.users[0];
+  let testPlatform: TestPlatformClient;
+
+  beforeAll(async () => {
+    testPlatform = await testPlatformClientFactory(mainUser.username, mainUser.password);
+  });
+
+  beforeEach(() => {
+    testPlatform.fakeToken = undefined;
+  });
+
   describe("getOne", () => {
-    let lensPlatformClient: LensPlatformClient;
-    let useInvalidToken: boolean;
+    it("rejects requests with invalid tokens", async () => {
+      testPlatform.fakeToken = "fake token";
 
-    beforeAll(async () => {
-      const client = new ResourceOwnerPassword({
-        client: {
-          id: config.keycloakClientId,
-          // Client secret not needed for Direct Access Grant / Resource Owner Password Credentials Grant
-          secret: "<client-secret>"
-        },
-        auth: {
-          tokenHost: config.tokenHost,
-          tokenPath: "/auth/realms/lensCloud/protocol/openid-connect/token"
-        }
-      });
-
-      const tokenParams = {
-        username: config.user.username,
-        password: config.user.password,
-        scope: "openid"
-      };
-
-      const accessToken = (await client.getToken(tokenParams)).token.access_token as string;
-
-      lensPlatformClient = new LensPlatformClient({
-        getAccessToken: () => useInvalidToken ? "invalid_token" : accessToken,
-        keyCloakAddress: config.keyCloakAddress,
-        keycloakRealm: config.keycloakRealm,
-        apiEndpointAddress: config.apiEndpointAddress
-      });
+      return expect(testPlatform.client.user.getOne({ username: mainUser.username }))
+        .rejects.toThrowError(UnauthorizedException);
     });
 
-    beforeEach(() => {
-      useInvalidToken = false;
+    it("can get itself", async () => {
+      const user = await testPlatform.client.user.getOne({ username: mainUser.username });
+
+      expect(user.username).toEqual(user.username);
     });
 
-    describe("getOne", () => {
-      it("rejects requiests with invalid tokens", async () => {
-        useInvalidToken = true;
+    it("throws NotFoundException if user is missing", async () => {
+      const username = "abcdef-12345-missing-" + String(Math.random() * 1000000000);
 
-        return expect(lensPlatformClient.user.getOne({ username: config.user.username }))
-          .rejects.toThrowError(UnauthorizedException);
-      });
+      return expect(testPlatform.client.user.getOne({ username }))
+        .rejects.toThrowError(NotFoundException);
+    });
+  });
 
-      it("can get itself", async () => {
-        const user = await lensPlatformClient.user.getOne({ username: config.user.username });
+  describe("updateOne", () => {
+    it("rejects requests with invalid tokens", async () => {
+      testPlatform.fakeToken = "fake token";
 
-        expect(user.username).toEqual(config.user.username);
-      });
-
-      it("throws NotFoundException if user is missing", async () => {
-        const username = "abcdef-12345-missing-" + String(Math.random() * 1000000000);
-
-        return expect(lensPlatformClient.user.getOne({ username }))
-          .rejects.toThrowError(NotFoundException);
-      });
+      return expect(testPlatform.client.user.updateOne(mainUser.username, {}))
+        .rejects.toThrowError(UnauthorizedException);
     });
 
-    describe("updateOne", () => {
-      it("rejects requiests with invalid tokens", async () => {
-        useInvalidToken = true;
-
-        return expect(lensPlatformClient.user.updateOne(config.user.username, {}))
-          .rejects.toThrowError(UnauthorizedException);
-      });
-
-      it("can update itself", async () => {
-        const user = await lensPlatformClient.user.updateOne(config.user.username, {});
-        expect(user.username).toEqual(config.user.username);
-      });
-
-      it("throws ForbiddenException when trying to modify unrelated users", async () => {
-        const username = "abcdef-12345-missing-" + String(Math.random() * 1000000000);
-
-        return expect(lensPlatformClient.user.updateOne(username, {}))
-          .rejects.toThrowError(ForbiddenException);
-      });
+    it("can update itself", async () => {
+      const user = await testPlatform.client.user.updateOne(mainUser.username, {});
+      expect(user.username).toEqual(mainUser.username);
     });
 
-    describe("getMany", () => {
-      it("rejects requiests with invalid tokens", async () => {
-        useInvalidToken = true;
+    it("throws ForbiddenException when trying to modify unrelated users", async () => {
+      const username = "abcdef-12345-missing-" + String(Math.random() * 1000000000);
 
-        return expect(lensPlatformClient.user.getMany())
-          .rejects.toThrowError(UnauthorizedException);
-      });
+      return expect(testPlatform.client.user.updateOne(username, {}))
+        .rejects.toThrowError(ForbiddenException);
+    });
+  });
 
-      it("can get users", async () => {
-        const users = await lensPlatformClient.user.getMany(`filter=email||$eq||${config.user.username}@mirantis.com`);
-        expect(users.length).toEqual(0);
-      });
+  describe("getMany", () => {
+    it("rejects requests with invalid tokens", async () => {
+      testPlatform.fakeToken = "fake token";
 
-      it("rejects bad requests", async () =>
-        expect(
-          lensPlatformClient.user.getMany()
-        ).rejects.toThrowError(BadRequestException)
-      );
+      return expect(testPlatform.client.user.getMany())
+        .rejects.toThrowError(UnauthorizedException);
+    });
+
+    it("can get users", async () => {
+      const users = await testPlatform.client.user.getMany(`filter=email||$eq||${mainUser.username}@mirantis.com`);
+      expect(users.length).toEqual(0);
+    });
+
+    it("rejects bad requests", async () =>
+      expect(
+        testPlatform.client.user.getMany()
+      ).rejects.toThrowError(BadRequestException)
+    );
+  });
+
+  describe("getSelf", () => {
+    it("rejects requiests with invalid tokens", async () => {
+      testPlatform.fakeToken = "fake token";
+
+      return expect(testPlatform.client.user.getSelf())
+        .rejects.toThrow();
+    });
+
+    it("can get self", async () => {
+      const user = await testPlatform.client.user.getSelf();
+      expect(user.username).toEqual(mainUser.username);
     });
   });
 });
