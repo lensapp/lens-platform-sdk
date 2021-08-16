08c05e8e5a7fbf59c520f444b5a4f5c4f205335d
Panu Horsmalahti <phorsmalahti@mirantis.com>
2021-08-16 15:52:10 +0300

    Add Permissions check for fetching hosted token. (#105)
    * Add Permissions check for fetching hosted token.
    
    * Rename hosted token endpoint

--- src/BillingPageTokenService.ts
+++ src/BillingPageTokenService.ts
@@ -1,25 +1,25 @@
 import { Base } from "./Base";
-import { ForbiddenException, SpaceNotFoundException, throwExpected, UnprocessableEntityException } from "./exceptions";
+import { ForbiddenException, SpaceNotFoundException, throwExpected, InternalServerException } from "./exceptions";
 
-export class HostedTokenService extends Base {
+export class BillingPageTokenService extends Base {
   /**
-   * Fetches a recurly's hosted page token
+   * Fetches Recurly's billing page token
    * required to access billing information controls.
-   * **IMPORTANT**: will fail with code `UnprocessableEntityException` when trying to fetch token for free spaces
+   * **IMPORTANT**: will fail with 500 when trying to fetch token for free spaces
    * due to a non-existent subscription information
    * @param spaceName - name of the space
-   * @returns hosted page token
+   * @returns billing page token
    */
   async getOne(spaceName: string): Promise<string> {
     const { apiEndpointAddress, fetch } = this.lensPlatformClient;
-    const url = `${apiEndpointAddress}/spaces/${spaceName}/hosted-token`;
+    const url = `${apiEndpointAddress}/spaces/${spaceName}/billing-page-token`;
 
     const token = await throwExpected(
       async () => fetch.get(url),
       {
         404: () => new SpaceNotFoundException(spaceName),
         403: () => new ForbiddenException("Token can only be acquired by space owners and admins"),
-        422: response => new UnprocessableEntityException(response?.body?.message ?? "Could not retrieve token")
+        500: response => new InternalServerException(response?.body?.message ?? "Could not retrieve token")
       }
     );
 
