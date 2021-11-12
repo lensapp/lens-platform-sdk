955908a22098f7f113bf9a82ba59e657d6f1df99
Juho Heikka <juho.heikka@gmail.com>
2021-11-12 12:00:40 +0200

    Refactor permissions switch fallthrough

--- src/Permissions.ts
+++ src/Permissions.ts
@@ -56,56 +56,73 @@ export class Permissions {
       invitationIdsCreatedByUserId: string[];
     }
   ) {
-    let canI = false;
+    const role = this.getRole(forSpace, forUserId);
+    const isAdminOrOwner = [Roles.Owner, Roles.Admin].includes(role);
+
+    const canPatchOrRevoceInvitation = () => {
+      if (isAdminOrOwner) {
+        return true;
+      }
+
+      if (
+        // If there is an invitationId to be revoked
+        forRevokeInvitation?.invitationId
+        // If this user has created more than one invitation
+        && forRevokeInvitation?.invitationIdsCreatedByUserId?.length > 0
+        // If invitation to revoke was created by userId
+        && forRevokeInvitation?.invitationIdsCreatedByUserId.find(
+          invitationIdCreatedByUserId => invitationIdCreatedByUserId === forRevokeInvitation?.invitationId
+        )
+      ) {
+        return true;
+      }
+
+      return false;
+    };
 
     switch (action) {
       case Actions.ChangeSpacePlan:
+        return role === Roles.Owner;
+
       case Actions.DeleteSpace:
-        canI = forSpace.kind !== "Personal" && this.getRole(forSpace, forUserId) === Roles.Owner;
-        break;
+        return forSpace.kind !== "Personal" && role === Roles.Owner;
+
       case Actions.PatchInvitation:
-      case Actions.RevokeInvitation: {
-        if ([Roles.Owner, Roles.Admin].includes(this.getRole(forSpace, forUserId))) {
-          canI = true;
-        }
+        return canPatchOrRevoceInvitation();
 
-        if (
-          // If there is an invitationId to be revoked
-          forRevokeInvitation?.invitationId
-          // If this user has created more than one invitation
-          && forRevokeInvitation?.invitationIdsCreatedByUserId?.length > 0
-          // If invitation to revoke was created by userId
-          && forRevokeInvitation?.invitationIdsCreatedByUserId.find(
-            invitationIdCreatedByUserId => invitationIdCreatedByUserId === forRevokeInvitation?.invitationId
-          )
-        ) {
-          canI = true;
-        }
+      case Actions.RevokeInvitation:
+        return canPatchOrRevoceInvitation();
 
-        break;
-      }
-
-      case Actions.RenameSpace: {
-        const canUpdate = [Roles.Owner, Roles.Admin].includes(this.getRole(forSpace, forUserId));
-
-        canI = forSpace.kind !== "Personal" && canUpdate;
-        break;
-      }
+      case Actions.RenameSpace:
+        return forSpace.kind !== "Personal" && isAdminOrOwner;
 
       case Actions.CreateInvitation:
-      case Actions.PatchTeam:
-      case Actions.PatchSpace:
-      case Actions.CreateTeam:
-      case Actions.DeleteTeam:
-      case Actions.AddInvitationDomain:
-      case Actions.GetBillingPageToken:
-      case Actions.DeleteInvitationDomain:
-      default:
-        canI = [Roles.Owner, Roles.Admin].includes(this.getRole(forSpace, forUserId));
-        break;
-    }
+        return isAdminOrOwner;
 
-    return canI;
+      case Actions.PatchTeam:
+        return isAdminOrOwner;
+
+      case Actions.PatchSpace:
+        return isAdminOrOwner;
+
+      case Actions.CreateTeam:
+        return isAdminOrOwner;
+
+      case Actions.DeleteTeam:
+        return isAdminOrOwner;
+
+      case Actions.AddInvitationDomain:
+        return isAdminOrOwner;
+
+      case Actions.GetBillingPageToken:
+        return isAdminOrOwner;
+
+      case Actions.DeleteInvitationDomain:
+        return isAdminOrOwner;
+
+      default:
+        return isAdminOrOwner;
+    }
   }
 
   /**
