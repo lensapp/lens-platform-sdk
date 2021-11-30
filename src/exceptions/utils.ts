9e784897bbd8c203cfb2528ee6a59f45d4532134
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2021-11-30 10:42:07 +0200

    Fix tests
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- src/exceptions/utils.ts
+++ src/exceptions/utils.ts
@@ -89,7 +89,7 @@ export const throwExpected = async <T = any>(fn: () => Promise<T>, exceptionsMap
     }
 
     // If axios.isAxiosError(error) returns falsy
-    throw new LensSDKException("Unknown", "Unexpected exception [Lens Platform SDK]", error);
+    throw new LensSDKException((error as any)?.response?.status, "Unexpected exception [Lens Platform SDK]", error);
   }
 };
 
