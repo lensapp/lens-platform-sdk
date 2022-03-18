8ef29f44e5a829d47858e3ab3e3c1b6c4355bf31
DmitriyNoa <dmytro.zharkov@gmail.com>
2022-03-18 16:13:22 +0100

    Add mapping between TeamEntityKind and Roles

--- src/UserRolesService.ts
+++ src/UserRolesService.ts
@@ -1,34 +1,48 @@
 import { Base } from "./Base";
 import { throwExpected } from "./exceptions";
 import { TeamEntityKind } from "./TeamService";
+import { Roles } from "./Permissions";
 
 interface UserSpaceRole {
+  role: Roles;
+}
+
+interface UserSpaceTeamEntity {
   role: TeamEntityKind;
 }
 
+const rolesToTeamEntityKindMap = {
+  [Roles.Owner]: "Owner",
+  [Roles.Admin]: "Admin",
+  [Roles.Member]: "Normal",
+  [Roles.None]: "Normal",
+};
+
+const teamEntityKindToRolesMap = {
+  ["Owner" as TeamEntityKind]: Roles.Owner,
+  ["Admin" as TeamEntityKind]: Roles.Admin,
+  ["Normal" as TeamEntityKind]: Roles.Member,
+};
+
 class UserRolesService extends Base {
-  async getUserSpaceRole(spaceName: string, userName: string) {
+  async changeUserSpaceRole(spaceName: string, userName: string, targetRole: Roles): Promise<UserSpaceRole> {
     const { apiEndpointAddress, fetch } = this.lensPlatformClient;
 
     const url = `${apiEndpointAddress}/spaces/${spaceName}/users/${userName}/role`;
 
-    const json = await throwExpected(
-      async () => fetch.get(url),
-    );
-
-    return (json as unknown) as UserSpaceRole;
-  }
-
-  async changeUserSpaceRole(spaceName: string, userName: string, targetRole: TeamEntityKind) {
-    const { apiEndpointAddress, fetch } = this.lensPlatformClient;
-
-    const url = `${apiEndpointAddress}/spaces/${spaceName}/users/${userName}/role`;
+    // Roles on the API side are TeamEntityKind. Map Roles to TeamEntityKind.
+    const teamRole = rolesToTeamEntityKindMap[targetRole];
 
     const json = await throwExpected(
-      async () => fetch.post(url, { role: targetRole }),
+      async () => fetch.post(url, { role: teamRole }),
     );
 
-    return (json as unknown) as UserSpaceRole;
+    const result = (json as unknown) as UserSpaceTeamEntity;
+
+    // TeamEntityKind on client are Roles. Map TeamEntityKind ro Roles.
+    return {
+      role: teamEntityKindToRolesMap[result.role],
+    };
   }
 }
 
