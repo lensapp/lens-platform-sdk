cdcb294338b1b22d43c479b1e9338648abe61ad5
DmitriyNoa <dmytro.zharkov@gmail.com>
2022-03-18 16:30:57 +0100

    Add more tests

--- src/UserRolesService.test.ts
+++ src/UserRolesService.test.ts
@@ -5,46 +5,56 @@ import { Roles } from "./Permissions";
 
 describe(".user role.*", () => {
   const username = "test-username";
-  const spacename = "test-space";
+  const spaceName = "test-space";
   const lensPlatformClient = new LensPlatformClient(options);
 
   it("Should change use role from Member to Admin", async () => {
-    nock(apiEndpointAddress).patch(`/spaces/${spacename}/users/${username}/role`).reply(200, {
+    nock(apiEndpointAddress).patch(`/spaces/${spaceName}/users/${username}/role`).reply(200, {
       role: "Admin",
     });
 
-    const role = await lensPlatformClient.roles.changeUserSpaceRole(spacename, username, Roles.Admin);
+    const role = await lensPlatformClient.roles.changeUserSpaceRole(spaceName, username, Roles.Admin);
 
     expect(role).toEqual({ role: Roles.Admin });
   });
 
   it("Should change use role from Member to Owner", async () => {
-    nock(apiEndpointAddress).patch(`/spaces/${spacename}/users/${username}/role`).reply(200, {
+    nock(apiEndpointAddress).patch(`/spaces/${spaceName}/users/${username}/role`).reply(200, {
       role: "Owner",
     });
 
-    const role = await lensPlatformClient.roles.changeUserSpaceRole(spacename, username, Roles.Owner);
+    const role = await lensPlatformClient.roles.changeUserSpaceRole(spaceName, username, Roles.Owner);
 
     expect(role).toEqual({ role: Roles.Owner });
   });
 
   it("Should change use role from Admin to Member", async () => {
-    nock(apiEndpointAddress).patch(`/spaces/${spacename}/users/${username}/role`).reply(200, {
+    nock(apiEndpointAddress).patch(`/spaces/${spaceName}/users/${username}/role`).reply(200, {
       role: "Normal",
     });
 
-    const role = await lensPlatformClient.roles.changeUserSpaceRole(spacename, username, Roles.Admin);
+    const role = await lensPlatformClient.roles.changeUserSpaceRole(spaceName, username, Roles.Admin);
 
     expect(role).toEqual({ role: Roles.Member });
   });
 
   it("Should change use role from Owner to Member", async () => {
-    nock(apiEndpointAddress).patch(`/spaces/${spacename}/users/${username}/role`).reply(200, {
+    nock(apiEndpointAddress).patch(`/spaces/${spaceName}/users/${username}/role`).reply(200, {
       role: "Normal",
     });
 
-    const role = await lensPlatformClient.roles.changeUserSpaceRole(spacename, username, Roles.Owner);
+    const role = await lensPlatformClient.roles.changeUserSpaceRole(spaceName, username, Roles.Owner);
 
     expect(role).toEqual({ role: Roles.Member });
+  });
+
+  it("Should change use role from Owner to Admin", async () => {
+    nock(apiEndpointAddress).patch(`/spaces/${spaceName}/users/${username}/role`).reply(200, {
+      role: "Admin",
+    });
+
+    const role = await lensPlatformClient.roles.changeUserSpaceRole(spaceName, username, Roles.Admin);
+
+    expect(role).toEqual({ role: Roles.Admin });
   });
 });
