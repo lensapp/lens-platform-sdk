672b05170848991adced63d830e42f99d9bc288b
Panu Horsmalahti <phorsmalahti@mirantis.com>
2022-04-21 11:16:38 +0300

    Add Reprovisioning state to DevCluster CRD.

--- src/K8sCluster.ts
+++ src/K8sCluster.ts
@@ -3,7 +3,7 @@ import type { MapToEntity } from "./types/types";
 import type { Except } from "type-fest";
 
 // State of the DevCluster CRD
-export type DevClusterCrdState = "Provisioning" | "Starting" | "Running" | "Stopping" | "Stopped" | "Terminating" | "Terminated";
+export type DevClusterCrdState = "Provisioning" | "Reprovisioning" | "Starting" | "Running" | "Stopping" | "Stopped" | "Terminating" | "Terminated";
 
 // Possible status.phase of the K8sCluster in the backend
 // This can be any CrdState and the others listed here.
