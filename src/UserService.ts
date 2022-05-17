90a9425a626ecd2c6c133d5626a469cfd8bee78b
lounjukk <{ID}+{username}@users.noreply.github.com>
2022-05-17 15:56:41 +0300

    getBillingPageToken resource for UserService

--- src/UserService.ts
+++ src/UserService.ts
@@ -31,6 +31,10 @@ export interface User {
 
 type UserWithEmail = User & { email: string };
 
+type BillingPageToken = {
+  hostedLoginToken: string;
+};
+
 export interface UserAttributes {
   fullname?: string;
   company?: string;
@@ -43,6 +47,7 @@ export interface UserAttributes {
  *
  * currently we have:
  * - `.getOne` maps to `GET /users/{username}`
+ * - `.getBillingPageToken` maps to `GET /users/{username}/billing-page-token`
  *
  * @remarks
  * This class should be generated using OpenAPI in the future.
@@ -131,6 +136,17 @@ class UserService extends Base {
         422: error => new UnprocessableEntityException(error?.body.message),
       },
     );
+  }
+
+  async getBillingPageToken({ username }: { username: string }): Promise<BillingPageToken> {
+    const { apiEndpointAddress, fetch } = this.lensPlatformClient;
+    const url = `${apiEndpointAddress}/users/${username}/billing-page-token`;
+    const json = await throwExpected(
+      async () => fetch.get(url),
+      { 404: () => new NotFoundException(`Billing page token for ${username} not found`) },
+    );
+
+    return (json as unknown) as BillingPageToken;
   }
 }
 
