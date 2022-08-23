9db26e68788b7fe7696691b8d3bc97bf01933d41
lounjukk <{ID}+{username}@users.noreply.github.com>
2022-08-23 10:16:22 +0300

    Make id optional when creating a business

--- src/BusinessService.ts
+++ src/BusinessService.ts
@@ -109,7 +109,7 @@ class BusinessService extends Base {
   /**
    * Create a new business ("Lens Business ID").
    */
-  async createOne(business: Business & { id?: string }): Promise<Business> {
+  async createOne(business: Omit<Business, "id"> & { id?: string }): Promise<Business> {
     const { apiEndpointAddress, fetch } = this.lensPlatformClient;
     const url = `${apiEndpointAddress}/businesses`;
     const json = await throwExpected(
