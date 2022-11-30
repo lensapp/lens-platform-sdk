1c80c5bd44aa9b1418a42d7994bfa3518ac3fa9a
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2022-11-30 14:28:26 +0200

    Add new 409 code for creating business invitation
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- src/BusinessService.ts
+++ src/BusinessService.ts
@@ -502,6 +502,7 @@ class BusinessService extends Base {
       async () => fetch.post(url, { email, subscriptionId, role }),
       {
         400: error => new BadRequestException(error?.body.message),
+        409: error => new BadRequestException(error?.body.message),
         422: error => new UnprocessableEntityException(error?.body.message),
         404: error => new NotFoundException(error?.body.message),
         403: error => new ForbiddenException(error?.body.message),
