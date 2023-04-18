8112c274d1724cecee2267c9b8893209d62dfdfc
DmitriyNoa <dmytro.zharkov@gmail.com>
2023-04-18 11:50:48 +0200

    Change to handle. Remove loginPrefixUrl

--- src/SSOService.ts
+++ src/SSOService.ts
@@ -1,10 +1,9 @@
 import { Base } from "./Base";
 import { NotFoundException, throwExpected } from "./exceptions";
-import { BusinessSSOWithIDPDetails } from "./BusinessService";
+import { Business, BusinessSSOWithIDPDetails } from "./BusinessService";
 
 export interface SSO {
   identityProviderID: string;
-  loginUrlPrefix: string;
 }
 
 class SSOService extends Base {
@@ -12,9 +11,9 @@ class SSOService extends Base {
    * Get SSO details
    *
    */
-  async getSSOByLoginUrlPrefix(loginUrlPrefix: SSO["loginUrlPrefix"]): Promise<SSO> {
+  async getSSOByBusinessHandle(handle: Business["handle"]): Promise<SSO> {
     const { apiEndpointAddress, fetch } = this.lensPlatformClient;
-    const url = `${apiEndpointAddress}/sso?loginUrlPrefix=${loginUrlPrefix}`;
+    const url = `${apiEndpointAddress}/sso?handle=${handle}`;
     const json = await throwExpected(async () => fetch.get(url), {
       404: () => new NotFoundException("SSO not found"),
     });
