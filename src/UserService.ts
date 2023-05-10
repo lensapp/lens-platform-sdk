882fa63d36917baa04c1070017c5e36c84a8bded
DmitriyNoa <dmytro.zharkov@gmail.com>
2023-05-10 16:09:25 +0200

    Add username to linked accounts

--- src/UserService.ts
+++ src/UserService.ts
@@ -277,6 +277,7 @@ export interface Invoice {
 export interface LinkedUserAccount {
   identityProviderAlias: string | undefined;
   identityProviderDisplayName: string | undefined;
+  userName: string | undefined;
 }
 
 /**
