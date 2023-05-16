05da8248119d5baf3dc88d400db5bd799e13d234
DmitriyNoa <dmytro.zharkov@gmail.com>
2023-05-16 09:52:51 +0200

    Add get oidc config method

--- src/SSOService.ts
+++ src/SSOService.ts
@@ -6,16 +6,39 @@ export interface SSO {
   identityProviderID: string;
 }
 
+export interface OIDCRemoteConfiguration {
+  authorizationUrl: string;
+  tokenUrl: string;
+  jwksUrl: string;
+  userInfoUrl: string;
+  issuer: string;
+  logoutUrl: string;
+}
+
 class SSOService extends Base {
   /**
    * Get SSO details
    *
    */
-  async getSSOByBusinessHandle(handle: Business["handle"]): Promise<SSO> {
+  async getSSOByBusinessHandle(handle: Business["handle"]): Promise<OIDCRemoteConfiguration> {
     const { apiEndpointAddress, fetch } = this.lensPlatformClient;
     const url = `${apiEndpointAddress}/sso?handle=${handle}`;
     const json = await throwExpected(async () => fetch.get(url), {
       404: () => new NotFoundException("SSO not found"),
+    });
+
+    return json as unknown as OIDCRemoteConfiguration;
+  }
+
+  /**
+   * Get OIDC SSO configuration
+   *
+   */
+  async getSSSOIDCConfiguration(oidcUrl: string): Promise<SSO> {
+    const { apiEndpointAddress, fetch } = this.lensPlatformClient;
+    const url = `${apiEndpointAddress}/configuration?url=${oidcUrl}`;
+    const json = await throwExpected(async () => fetch.get(url), {
+      422: () => new NotFoundException("OIDC configuration is not available or invalid"),
     });
 
     return json as unknown as BusinessSSOWithIDPDetails;
