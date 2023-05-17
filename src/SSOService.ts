fd6f7a1d6036b5c667436e9ee410bd9cabb179d4
DmitriyNoa <dmytro.zharkov@gmail.com>
2023-05-17 09:08:43 +0200

    Connect SSO provider SDK

--- src/SSOService.ts
+++ src/SSOService.ts
@@ -1,9 +1,13 @@
 import { Base } from "./Base";
-import { NotFoundException, throwExpected } from "./exceptions";
+import { ForbiddenException, NotFoundException, throwExpected } from "./exceptions";
 import { Business, BusinessSSOWithIDPDetails } from "./BusinessService";
 
 export interface SSO {
   identityProviderID: string;
+}
+
+export interface SSOProviderConnection {
+  link: string;
 }
 
 class SSOService extends Base {
@@ -19,6 +23,26 @@ class SSOService extends Base {
     });
 
     return json as unknown as BusinessSSOWithIDPDetails;
+  }
+
+  /**
+   * Get SSO provider connection link
+   *
+   */
+  async getSSOProviderConnectionLink(
+    providerAlias: string,
+    clientId: string,
+  ): Promise<SSOProviderConnection> {
+    const { apiEndpointAddress, fetch } = this.lensPlatformClient;
+    const url = `${apiEndpointAddress}/account-link?provider=${providerAlias}&clientId=${clientId}`;
+    const json = await throwExpected(async () => fetch.get(url), {
+      404: () => new NotFoundException("SSO provider found"),
+      403: (error) => new ForbiddenException(error?.body.message),
+      401: (error) => new ForbiddenException(error?.body.message),
+      422: () => new NotFoundException(`Failed to retrieve link for provider ${providerAlias}`),
+    });
+
+    return json as unknown as SSOProviderConnection;
   }
 }
 
