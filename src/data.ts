95b0f7dbecaa7ed4dbe890b1aec56691f111d1bb
Sami Tiilikainen <97873007+samitiilikainen@users.noreply.github.com>
2023-12-29 11:56:31 +0200

    Remove grace period
    Signed-off-by: Sami Tiilikainen <97873007+samitiilikainen@users.noreply.github.com>

--- src/data.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Grace period for existing users (before License Change release)
- * After this date, existing users will be required to choose a license subscription.
- * Before this date, existing users will be able to use Lens without a valid subscription.
- *
- * Mon Jan 02 2023 03:00:00 GMT+0000 (1672628400000)
- */
-export const endOfGracePeriod = Date.UTC(2023, 0, 2, 3, 0, 0, 0);
