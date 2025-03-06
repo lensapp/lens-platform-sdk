da4dc84e5acf27fe2480d409e6f17708a7d88e20
Fedor Novikov <fnovikov@mirantis.com>
2025-03-06 13:45:44 +0100

    feat: added api for gradual rollout

--- src/DomainFeaturesService.test.ts
+++ src/DomainFeaturesService.test.ts
@@ -3,22 +3,13 @@ import LensPlatformClient from "./LensPlatformClient";
 import { minimumOptions as options, apiEndpointAddress } from "./LensPlatformClient.test";
 
 describe(".domain/features.*", () => {
-  const username = "test-username";
   const domain = "mirantis.com";
 
-  nock(apiEndpointAddress)
-    .get("/domain/features")
-    .reply(200, [
-      {
-        domainMatchingEnabled: false,
-      },
-    ]);
+  nock(apiEndpointAddress).get(`/domain/features?domain=${domain}`).reply(200, {
+    domainMatchingEnabled: true,
+  });
 
   const lensPlatformClient = new LensPlatformClient(options);
-
-  lensPlatformClient.getDecodedAccessToken = jest.fn().mockResolvedValue({
-    preferred_username: username,
-  });
 
   it("can call get", async () => {
     const features = await lensPlatformClient.domainFeatures.get({ domain });
