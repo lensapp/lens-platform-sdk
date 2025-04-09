160e5e0fa581e84cc29d7a12b266c83098f46ded
lounjukk <1618357+lounjukk@users.noreply.github.com>
2025-04-09 10:54:46 +0300

    Add user OTP preferences endpoint

--- src/UserService.ts
+++ src/UserService.ts
@@ -59,6 +59,10 @@ export interface UserCommunications {
   onboarding?: boolean;
 }
 
+export interface UserOTPPreferences {
+  enabled: boolean;
+}
+
 export type SubscriptionState = "active" | "canceled" | "expired" | "failed" | "future" | "paused";
 
 export type UserBusinessWithSSOInfo = { role: UserBusinessRole } & Pick<
@@ -680,6 +684,38 @@ class UserService extends Base {
     });
 
     return json as unknown as LinkedUserAccount[];
+  }
+
+  /**
+   * Get user one time password preferences
+   */
+  async getUserOTPPreferences() {
+    const { apiEndpointAddress, fetch } = this.lensPlatformClient;
+    const username = await this.getUsername();
+    const url = `${apiEndpointAddress}/users/${username}/otp`;
+    const json = await throwExpected(async () => fetch.get(url), {
+      401: (error) => new UnauthorizedException(error?.body?.message),
+      403: (error) => new UnauthorizedException(error?.body?.message),
+      404: (error) => new NotFoundException(error?.body?.message),
+    });
+
+    return json as unknown as UserOTPPreferences[];
+  }
+
+  /**
+   * Update user one time password preferences
+   */
+  async updateUserOTPPreferences(preferences: UserOTPPreferences) {
+    const { apiEndpointAddress, fetch } = this.lensPlatformClient;
+    const username = await this.getUsername();
+    const url = `${apiEndpointAddress}/users/${username}/otp`;
+    const json = await throwExpected(async () => fetch.patch(url, preferences), {
+      401: (error) => new UnauthorizedException(error?.body?.message),
+      403: (error) => new UnauthorizedException(error?.body?.message),
+      404: (error) => new NotFoundException(error?.body?.message),
+    });
+
+    return json as unknown as UserOTPPreferences;
   }
 
   /**
